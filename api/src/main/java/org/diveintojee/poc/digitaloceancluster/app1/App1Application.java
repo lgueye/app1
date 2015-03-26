208900205cd5e9aa898eb2a5ce138f77e5b6a391
louis gueye <louis.gueye@gmail.com>
2015-03-27 00:59:56 +0100

    Remove unnecessary annotation

--- api/src/main/java/org/diveintojee/poc/digitaloceancluster/app1/App1Application.java
+++ api/src/main/java/org/diveintojee/poc/digitaloceancluster/app1/App1Application.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "org.diveintojee.poc.digitaloceancluster")
 @EnableJpaRepositories(basePackages = "org.diveintojee.poc.digitaloceancluster.app1.persistence.data")
