9324ea56b87eee46baca27aa7b5e602b292332e9
louis gueye <louis.gueye@gmail.com>
2015-04-21 01:20:43 +0200

    Rewrite crud test in bulk mode

--- tests/src/test/java/org/diveintojee/poc/digitaloceancluster/app1/DomainResourceTestIT.java
+++ tests/src/test/java/org/diveintojee/poc/digitaloceancluster/app1/DomainResourceTestIT.java
@@ -1,5 +1,7 @@
 package org.diveintojee.poc.digitaloceancluster.app1;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import org.diveintojee.poc.digitaloceancluster.app1.domain.Domain;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,43 +27,98 @@ public class DomainResourceTestIT {
 
     @Test
     public void crudDomainShouldSucceed() throws IOException {
-        for (int i = 0; i < 100; i++ ) {
-            performCrudTest();
+        List<Domain> detachedList = Lists.newArrayList();
+        final int countInstances = 100;
+        for (int i = 0; i < countInstances; i++ ) {
+            detachedList.add(Fixtures.validDomain());
         }
+        List<URI> uriList = Lists.newArrayList();
+        for (Domain domain : detachedList) {
+            final URI uri = api.createDomain(domain);
+            assertNotNull(uri);
+            uriList.add(uri);
+        }
+        List<Domain> persistedList = Lists.newArrayList();
+        for (URI uri : uriList) {
+            final Domain persisted = api.loadDomain(uri);
+            assertNotNull(persisted);
+            persistedList.add(persisted);
+        }
+        List<Domain> mergedList = Lists.newArrayList();
+        for (int i = 0; i < countInstances; i++) {
+            Domain merged = new Domain();
+            merged.setId(persistedList.get(i).getId());
+            merged.setDescription(detachedList.get(i).getDescription());
+            merged.setTitle(detachedList.get(i).getTitle());
+            mergedList.add(merged);
+        }
+        assertEquals(Sets.newHashSet(mergedList), Sets.newHashSet(persistedList));
+        List<Domain> copyFromPersisted = Lists.newArrayList();
+        final String brandNewTitle = "Brand new awesome title";
+        for (Domain persisted : persistedList) {
+            Domain copy = new Domain();
+            copy.setId(persisted.getId());
+            copy.setDescription(persisted.getDescription());
+            copy.setTitle(brandNewTitle);
+            copyFromPersisted.add(copy);
+        }
+        for (int i = 0; i < countInstances; i++) {
+            api.updateDomain(uriList.get(i), copyFromPersisted.get(i));
+        }
+        List<Domain> updatedList = Lists.newArrayList();
+        for (URI uri : uriList) {
+            final Domain updated = api.loadDomain(uri);
+            assertNotNull(updated);
+            updatedList.add(updated);
+        }
+        assertEquals(Sets.newHashSet(copyFromPersisted), Sets.newHashSet(updatedList));
+        for (URI uri : uriList) {
+            api.deleteResource(uri);
+        }
+        for (URI uri : uriList) {
+            try {
+                new RestTemplate().getForEntity(uri, Domain.class);
+            } catch (HttpClientErrorException e) {
+                assertEquals(HttpStatus.NOT_FOUND, e.getStatusCode());
+            } catch (Exception e) {
+                fail("expected {" + HttpClientErrorException.class + "}, got {" + e + "}");
+            }
+        }
+
     }
 
-    private void performCrudTest() throws IOException {
-        Domain domain = Fixtures.validDomain();
-        // Create
-        URI uri = api.createDomain(domain);
-        assertNotNull(uri);
-
-        // Read
-        Domain persisted = api.loadDomain(uri);
-        assertNotNull(persisted);
-        Long id = persisted.getId();
-        assertNotNull(id);
-        persisted.setId(null);
-        assertEquals(persisted, domain);
-
-        // Update
-        persisted.setId(id);
-        String expectedName = "test test test";
-        persisted.setTitle(expectedName);
-        api.updateDomain(uri, persisted);
-        Domain updated = api.loadDomain(uri);
-        assertEquals(persisted, updated);
-
-        // Delete
-        api.deleteResource(uri);
-        try {
-            new RestTemplate().getForEntity(uri, Domain.class);
-        } catch (HttpClientErrorException e) {
-            assertEquals(HttpStatus.NOT_FOUND, e.getStatusCode());
-        } catch (Exception e) {
-            fail("expected {" + HttpClientErrorException.class + "}, got {" + e + "}");
-        }
-    }
+//    private void performCrudTest() throws IOException {
+//        Domain domain = Fixtures.validDomain();
+//        // Create
+//        URI uri = api.createDomain(domain);
+//        assertNotNull(uri);
+//
+//        // Read
+//        Domain persisted = api.loadDomain(uri);
+//        assertNotNull(persisted);
+//        Long id = persisted.getId();
+//        assertNotNull(id);
+//        persisted.setId(null);
+//        assertEquals(persisted, domain);
+//
+//        // Update
+//        persisted.setId(id);
+//        String expectedName = "test test test";
+//        persisted.setTitle(expectedName);
+//        api.updateDomain(uri, persisted);
+//        Domain updated = api.loadDomain(uri);
+//        assertEquals(persisted, updated);
+//
+//        // Delete
+//        api.deleteResource(uri);
+//        try {
+//            new RestTemplate().getForEntity(uri, Domain.class);
+//        } catch (HttpClientErrorException e) {
+//            assertEquals(HttpStatus.NOT_FOUND, e.getStatusCode());
+//        } catch (Exception e) {
+//            fail("expected {" + HttpClientErrorException.class + "}, got {" + e + "}");
+//        }
+//    }
 
 
     @Test
